f90aff8a9b7efce213e9c7f7694fa3b687a6441a
高干 <779322720@qq.com>
2023-04-27 14:41:10 +0800

    s

---
+++ tests/PushTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ *  TestSms.php
+ *
+ * @author szm19920426@gmail.com
+ * $Id: TestSms.php 2017-08-17 上午10:08 $
+ */
+
+namespace ShaoZeMing\GeTui\Test;
+require_once dirname(__FILE__) . '/../src/getui/IGt.Push.php';
+
+use PHPUnit\Framework\TestCase;
+use ShaoZeMing\GeTui\GeTuiService;
+
+
+class PushTest extends TestCase
+{
+    protected $instance;
+
+    public function setUp()
+    {
+
+        $file =  dirname(__DIR__) .'/src/config/getui.php';
+        $config = include($file);
+        $this->instance = new GeTuiService($config);
+    }
+
+
+    public function testPushManager()
+    {
+        $this->assertInstanceOf(GeTuiService::class, $this->instance);
+    }
+
+
+    public function testPush()
+    {
+        echo PHP_EOL."发送push 中....".PHP_EOL;
+        try {
+            $deviceId = '2a93cba4f7e7f47e367ae7d65c961bd3';
+            $title = '噪音好大啊，啊，啊，啊啊，';
+            $content = '你好呀您负责的的工单已经追加元';
+
+            $data = [
+                'type' => 9,
+                'title' => $title,
+                'content' => $content,
+            ];
+
+            $getuiResponse = $this->instance->push($deviceId, $data);
+//            $getuiResponse = $this->instance->pushToApp( $data);
+            echo json_encode($getuiResponse).PHP_EOL;
+//            $this->assertContains('ok',$getuiResponse,'不成功');
+//            return $getuiResponse;
+        } catch (\Exception $e) {
+
+
+            $err = "Error : 错误：" . $e->getMessage();
+            echo $err.PHP_EOL;
+
+        }
+    }
+}
